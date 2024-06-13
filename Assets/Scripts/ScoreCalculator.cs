ef0e3e6e662c0707216f60ba613103c702b2766f
LStefanCoder <you@example.com>
2024-06-13 08:57:33 +0200

    Continued to model the garage and added scores for other players.

---
+++ Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreCalculator : MonoBehaviour
+{
+
+    public Sprite Player1;
+    public Sprite Player2;
+    public Sprite Player3;
+    public Sprite Player4;
+    public Sprite User;
+    public TMP_Text TotalScore;
+
+    //since the other script is attached to the same game object, I can reference it here
+    //see https://discussions.unity.com/t/help-how-do-i-reference-access-another-script-in-unity-c/156123/2
+    //ReferencedScript 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeImagePositions(int score)
+    {
+        if(score > )
+    }
+}
+
