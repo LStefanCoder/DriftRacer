1d851162418239813d12f78c11b7ae97e57ad892
LStefanCoder <you@example.com>
2024-09-01 19:06:27 +0200

    Created finish line script and added a start line.

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    public GameObject mainCanvas;
+    public GameObject finishCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +20,39 @@ public class FinishLine : MonoBehaviour
         
     }
 
+    void FinishLine()
+    {
+        mainCanvas.SetActive(false);
+        finishCanvas.SetActive(true);
+    }
+
+    void onContinue()
+    {
+        //unfreezing game
+        Time.timeScale = 1;
+        if (SceneManager.GetActiveScene() == "Level1")
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
+        if (SceneManager.GetActiveScene() == "Level2")
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
     //this is a built-in function, see https://docs.unity3d.com/ScriptReference/Collider.OnTriggerEnter.html
     //inspired by https://www.youtube.com/watch?v=lmbJiV8ZETc
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger hit");
+        //only trigger the finish line script for the finish line itself, and not other colliders
+        if (other.gameObject.CompareTag("FinishLine"))
+        {
+            //freezing game
+            Time.timeScale = 0;
+            //displaying finish screen
+            FinishLine();
+        }
     }
 }
 
